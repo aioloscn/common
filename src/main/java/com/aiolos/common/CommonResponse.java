ad88851843aa8879aed996cd0fe4560ee4db3f22
Aioloscn <15988346908@163.com>
2021-01-14 04:46:23 +0800

    initialization

---
+++ src/main/java/com/aiolos/common/CommonResponse.java
@@ -0,0 +1,115 @@
+package com.aiolos.common;
+
+import com.aiolos.common.enums.ErrorEnum;
+
+import java.io.Serializable;
+
+/**
+ * 自定义响应数据结构
+ * 状态码定义在ErrorEnum类中，200：成功（默认），500：错误（默认）
+ * @author Aiolos
+ * @date 2020/9/22 1:11 下午
+ */
+public class CommonResponse<T> implements Serializable {
+
+    // 响应业务状态码
+    private Integer code;
+
+    // 响应信息
+    private String msg;
+
+    // 响应中的数据
+    private T data;
+
+    private CommonResponse() {
+        this.code = 200;
+        this.msg = "SUCCESS";
+    }
+
+    private CommonResponse(String msg) {
+        this.code = 200;
+        this.msg = msg;
+    }
+
+    private CommonResponse(T data) {
+        this.code = 200;
+        this.msg = "SUCCESS";
+        this.data = data;
+    }
+
+    private CommonResponse(String msg, T data) {
+        this.code = 200;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static CommonResponse ok() {
+        return new CommonResponse();
+    }
+
+    public static CommonResponse ok(String msg) {
+        return new CommonResponse(msg);
+    }
+
+    public static CommonResponse ok(Object data) {
+        return new CommonResponse(data);
+    }
+
+    public static CommonResponse ok(String msg, Object data) {
+        return new CommonResponse(msg, data);
+    }
+
+    public static CommonResponse error(Integer errCode, String errMsg) {
+        CommonResponse res = new CommonResponse();
+        res.code = errCode;
+        res.msg = errMsg;
+        return res;
+    }
+
+    public static CommonResponse error(String errMsg, Object data) {
+        CommonResponse res = new CommonResponse();
+        res.code = 500;
+        res.msg = errMsg;
+        res.data = data;
+        return res;
+    }
+
+    public static CommonResponse error(Integer errCode, String errMsg, Object data) {
+        CommonResponse res = new CommonResponse();
+        res.code = errCode;
+        res.msg = errMsg;
+        res.data = data;
+        return res;
+    }
+
+    public static CommonResponse error(ErrorEnum errorEnum) {
+        CommonResponse res = new CommonResponse();
+        res.code = errorEnum.getErrCode();
+        res.msg = errorEnum.getErrMsg();
+        return res;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/aiolos/common/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,63 @@
+package com.aiolos.common.advice;
+
+import com.aiolos.common.CommonResponse;
+import com.aiolos.common.exception.CustomizeException;
+import com.aiolos.common.enums.ErrorEnum;
+import com.aiolos.common.utils.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 所有执行的controller都会被这个切面所包含
+ * @author Aiolos
+ * @date 2020/10/10 4:14 下午
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    /**
+     * 定义ExceptionHandler解决为被controller层吸收的exception
+     * 只要对应的controller抛出了任何Exception或者继承自Exception的异常，都会做一个对应的处理
+     * @return  返回封装好的公共web对象
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public CommonResponse handlerCustomizeException(HttpServletRequest req, HttpServletResponse res, Exception e) {
+
+        if (e instanceof CustomizeException) {
+            log.info("全局异常捕获，异常信息：{}", ((CustomizeException) e).getErrMsg());
+            return CommonResponse.error(((CustomizeException) e).getErrCode(), ((CustomizeException) e).getErrMsg());
+        } else {
+            log.info(e.getMessage());
+            if (e instanceof NoHandlerFoundException) {
+                return CommonResponse.error(ErrorEnum.NO_HANDLER_FOUND);
+            } else if (e instanceof MethodArgumentNotValidException) {
+
+                BindingResult bindingResult = ((MethodArgumentNotValidException) e).getBindingResult();
+                return CommonResponse.error(ErrorEnum.PARAMETER_VALIDATION_ERROR.getErrCode(), CommonUtils.processErrorString(bindingResult));
+            } else if (e instanceof ServletRequestBindingException) {
+                return CommonResponse.error(ErrorEnum.BIND_EXCEPTION_ERROR);
+            } else if (e instanceof NullPointerException) {
+                return CommonResponse.error(ErrorEnum.NULL_POINT_ERROR);
+            } else if (e instanceof MaxUploadSizeExceededException) {
+                return CommonResponse.error(ErrorEnum.FILE_MAX_SIZE_ERROR);
+            } else {
+                return CommonResponse.error(ErrorEnum.UNKNOWN_ERROR);
+            }
+        }
+    }
+}
