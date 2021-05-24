ab7838560dacac4b59560a1291753584e3761c09
Aioloscn <756243011@qq.com>
2021-05-24 12:33:43 +0800

    Added a exception handler

---
+++ src/main/java/com/aiolos/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.aiolos.common.advice;
+
+import com.aiolos.common.enums.ErrorEnum;
+import com.aiolos.common.response.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Aiolos
+ * @date 2021/4/17 9:52 下午
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public CommonResponse handlerException(HttpServletRequest req, HttpServletResponse resp, Exception e) {
+        log.warn("全局异常捕获：{}", e.getMessage());
+        if (e instanceof NoHandlerFoundException) {
+            return CommonResponse.error(ErrorEnum.NO_HANDLER_FOUND);
+        } else if (e instanceof ServletRequestBindingException) {
+            return CommonResponse.error(ErrorEnum.BIND_EXCEPTION_ERROR);
+        } else if (e instanceof NullPointerException) {
+            return CommonResponse.error(ErrorEnum.NULL_POINT_ERROR);
+        } else if (e instanceof MaxUploadSizeExceededException) {
+            return CommonResponse.error(ErrorEnum.FILE_MAX_SIZE_ERROR);
+        } else {
+            return CommonResponse.error(ErrorEnum.UNKNOWN_ERROR);
+        }
+    }
+}
